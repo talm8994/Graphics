e17c296424c61bf50352888a8923899ec8d3de78
talm8994 <tom.tom.pop13@gmail.com>
2016-04-26 12:39:16 -0600

    added new lists

--- src/view/ShapePanel.java
+++ src/view/ShapePanel.java
@@ -1,5 +1,9 @@
 package view;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Polygon;
 import java.awt.Rectangle;
 import java.awt.geom.Ellipse2D;
@@ -17,7 +21,7 @@ public class ShapePanel extends JPanel
 	
 	public ShapePanel()
 	{
-		triangleList = new ArrayLIst<>
+		
 	}
 	
 	public void addRectangle()
@@ -30,8 +34,26 @@ public class ShapePanel extends JPanel
 	}
 	public void addEllips()
 	{
-		
+		 int xPosition = (int)(Math.random() * 1500);
+		 int yPosition = (int)(Math.random() * 2000);
 	}
+	
+	public void addPolygon()
+	{
+		int numberOfSides = (int)(Math.random() * 9) +3;
+		int [] xPoints = new int [numberOfSides];
+		int [] yPoints = new int [numberOfSides];
+		
+		for (int side = 0; side < numberOfSides; side++)
+		{
+			xPoints[side] = (int)(Math.random() * 2000);
+			yPoints[side] = (int)(Math.random() * 2000);
+		}
+		
+		Polygon myPolygon = new Polygon(xPoints, yPoints, numberOfSides);
+		polygonList.add(myPolygon);
+	}
+	
 	public void addTriangle()
 	{
 		int [] xPoints = new int[3];
@@ -43,5 +65,32 @@ public class ShapePanel extends JPanel
 		
 		Polygon triangle = new Polygon(xPoints, yPoints, 3);
 		triangleList.add(triangle);
+	}
+	
+	protected void paintConponent(Graphics currentGraphics)
+	{
+		super.paintComponent(currentGraphics);;
+		Graphics2D mainGraphics = (Graphics2D) currentGraphics;
+		
+		for(Polygon currentTriangle : triangleList)
+		{
+			int red = (int)(Math.random()* 256);
+			int green = (int)(Math.random()* 256);
+			int blue = (int)(Math.random()* 256);
+			
+			int penSize = (int)(Math.random()* 10) + 3;
+			mainGraphics.setColor(new Color(red, green, blue));
+			mainGraphics.setStroke(new BasicStroke(penSize));
+			
+			mainGraphics.draw(currentTriangle);
+		}
+		for(Polygon current : polygonList)
+		{
+			int red = (int)(Math.random()* 256);
+			int green = (int)(Math.random()* 256);
+			int blue = (int)(Math.random()* 256);
+			
+			mainGraphics.setColor(new Color(red, green, blue));
+		}
 	}
 }
