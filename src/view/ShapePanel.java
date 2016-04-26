138109ab98a2eab3a8431101f0af26d21382fc1f
talm8994 <tom.tom.pop13@gmail.com>
2016-04-26 12:03:26 -0600

    as

--- src/view/ShapePanel.java
+++ src/view/ShapePanel.java
@@ -15,6 +15,10 @@ public class ShapePanel extends JPanel
 	private ArrayList<Polygon> triangleList;
 	private ArrayList<Polygon> polygonList;
 	
+	public ShapePanel()
+	{
+		triangleList = new ArrayLIst<>
+	}
 	
 	public void addRectangle()
 	{
@@ -27,5 +31,17 @@ public class ShapePanel extends JPanel
 	public void addEllips()
 	{
 		
+	}
+	public void addTriangle()
+	{
+		int [] xPoints = new int[3];
+		xPoints[0] = (int)(Math.random() * 150);
+		xPoints[1] = (int)(Math.random() * 250);
+		xPoints[2] = (int)(Math.random() * 350);
+		
+		int [] yPoints = {(int)(Math.random() * 150), (int)(Math.random() * 250), (int)(Math.random() * 350)};
+		
+		Polygon triangle = new Polygon(xPoints, yPoints, 3);
+		triangleList.add(triangle);
 	}
 }
